629d2e650acddc3582cd70c5b858252684b9e29d
Edmon Bosch <edmonbosch@fundacionord.cat>
2022-10-13 13:00:59 +0200

    loading uncommented

--- lib/src/data/providers/remote/authentication_provider.dart
+++ lib/src/data/providers/remote/authentication_provider.dart
@@ -5,11 +5,11 @@ import '../../models/user.dart';
 class AuthenticationProvider {
 
   Future<User> login(String email, String password) async {
-    // await Future.delayed(
-    //   const Duration(
-    //     seconds: 3,
-    //   ),
-    // );
+    await Future.delayed(
+      const Duration(
+        seconds: 3,
+      ),
+    );
     
     if(email == "test@test.com" && password == "test") {
       return User(
